43d2bcba9ca558bcd850c805f2e70f8dda300e1f
afifafr <tayeb.afifa@gmail.com>
2023-11-08 13:04:47 +0100

    first commit

---
+++ src/product.js
@@ -0,0 +1,18 @@
+const product = {
+    name: "IPHONE 15 PRO MAX",
+    price: "6999 dt",
+    description: `
+    Écran Super Retina XDR de 6,7" 
+    -Écran toujours activé 
+    -Technologie ProMotion avec taux de rafraîchissement adaptatif atteignant 120 Hz
+    -Résolution: 2 796 x 1 290 pixels 
+    -Dynamic Island 
+    -True Tone  
+    -Haptic Touch 
+    -Contraste 2 000 000:1 (standard) 
+    -Appareil photo 12 Mpx Ouverture ƒ/1,9 - Garantie: 1an
+    `,
+    image: "https://www.tryandbuy.tn/46581-large_default/iphone-15-pro-max-prix-tunisie.jpg",
+  };
+  
+  export default product;
